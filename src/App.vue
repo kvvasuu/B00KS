910ef318f5f63a17ca930c28c974870dff3d2c70
kvvasu <lukaszkwas96@gmail.com>
2024-05-15 22:28:06 +0200

    Initial

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="header">
+    <div class="input-container">
+      <h2>Choose category</h2>
+      <select class="select-category" name="categories">
+        <option v-for="category in categories" value="category">
+          {{ category }}
+        </option>
+      </select>
+    </div>
+  </div>
+  <div class="items-container">
+    <item-card></item-card>
+    <item-card></item-card>
+    <item-card></item-card>
+    <item-card></item-card>
+    <item-card></item-card>
+    <item-card></item-card>
+  </div>
+</template>
+
+<script>
+import ItemCard from "./components/ItemCard.vue";
+export default {
+  components: {
+    ItemCard,
+  },
+  data() {
+    return {
+      categories: ["ab", "adsa", "rtyetre", "dsade"],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  height: 10rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.input-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.select-category {
+  appearance: none;
+  border: 0;
+  outline: 0;
+  font: inherit;
+  width: 20rem;
+  padding: 1rem;
+  background: rgba(255, 255, 255, 0.5);
+  border-radius: 0.25em;
+  box-shadow: 0 0 1em 0 rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+}
+
+.items-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+</style>
---
+++ src/components/ItemCard.vue
@@ -0,0 +1,42 @@
+<script></script>
+
+<template>
+  <div class="card">
+    <div class="book-image-container">
+      <img src="../assets/home2.jpg" class="book-no-image" />
+    </div>
+    <div class="author"></div>
+    <div class="description">
+      <div class="title"></div>
+      <div class="caption"></div>
+    </div>
+    <div class="buy-container">
+      <div class="buy-buttons"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.book-image-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.book-no-image {
+  font-size: 4rem;
+  font-weight: bold;
+  display: block;
+  transform: perspective(1500px) rotateY(20deg);
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 25px 50px -12px;
+  transition: transform 1s ease 0s;
+}
+.card {
+  background-color: #ffffff;
+  height: 30rem;
+  width: 20rem;
+  border: 2px solid rgba(0, 0, 0, 0.274);
+  border-radius: 1rem;
+  margin: 2rem;
+}
+</style>
